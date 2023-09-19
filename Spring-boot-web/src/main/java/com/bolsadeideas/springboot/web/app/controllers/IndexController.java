79ab53471ed2b392e4632fbfc0c31ec2b2011e52
Isael Jimenez <isaeljimenez94@gmail.com>
2023-09-18 22:42:36 -0600

    Se agrega el paquete model con una clase pojo usuario para mostrarlo en la vista perfil

--- Spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
+++ Spring-boot-web/src/main/java/com/bolsadeideas/springboot/web/app/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package com.bolsadeideas.springboot.web.app.controllers;
 
+import com.bolsadeideas.springboot.web.app.models.Usuario;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,9 +10,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/app")
 public class IndexController {
-    @GetMapping({"/index", "/"," ", "/home"})
+    @GetMapping({"/index", "/", " ", "/home"})
     public String index(Model model) {
-        model.addAttribute("titulo","Hola Spring Framework!");
+        model.addAttribute("titulo", "Hola Spring Framework!");
         return "index";
+    }
+
+    @RequestMapping("/perfil")
+    public String perfil(Model model) {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Isael");
+        usuario.setApellido("Jimenez");
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", "Perfil de usuario: ".concat(usuario.getNombre()));
+        return "perfil";
     }
 }
