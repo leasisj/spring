1233673e8e89e179b1b07e89e5831eb9b34bd2dd
Isael Jimenez <isaeljimenez94@gmail.com>
2023-09-20 08:31:58 -0600

    Nuevo proyecto: Relaciones entre componentes

---
+++ Seccion2/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/domain/Factura.java
@@ -0,0 +1,41 @@
+package com.bolsadeideas.springboot.di.app.models.domain;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+@Component
+public class Factura {
+
+    @Value("${descripcion.factura}")
+    private String descripcion;
+
+    @Autowired
+    private Cliente cliente;
+    private List<itemFactura> items;
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public List<itemFactura> getItems() {
+        return items;
+    }
+
+    public void setItems(List<itemFactura> items) {
+        this.items = items;
+    }
+}
