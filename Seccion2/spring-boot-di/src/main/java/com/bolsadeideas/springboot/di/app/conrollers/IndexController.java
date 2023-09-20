4aeea42418c1069f46b325879153cf535af175a8
Isael Jimenez <isaeljimenez94@gmail.com>
2023-09-19 22:17:29 -0600

    Inyeccion de dependencia por el metodo contructor, seter colocando @Autowired

--- Seccion2/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/conrollers/IndexController.java
+++ Seccion2/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/conrollers/IndexController.java
@@ -9,13 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
-    @Autowired
+    //@Autowired
     private IServicio servicio;
+
+    //@Autowired
+    public IndexController(IServicio servicio) {
+        this.servicio = servicio;
+    }
     @GetMapping({"/index", "/", ""})
     public String index(Model model) {
         String operacion = servicio.operacion();
         model.addAttribute("titulo", "Haciendo prueba de isael");
         model.addAttribute("texto", "La operacion realizada es: " + operacion);
         return "index";
+    }
+    //@Autowired
+    public void setServicio(IServicio servicio) {
+        this.servicio = servicio;
     }
 }
