f55f7ab5262b592c2c958c70a3ad8cfb71d23472
Isael Jimenez <isaeljimenez94@gmail.com>
2023-09-20 14:29:30 -0600

    Se agrego despues de el ID

--- Seccion2/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/conrollers/facturaControl.java
+++ Seccion2/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/conrollers/facturaControl.java
@@ -2,7 +2,6 @@ package com.bolsadeideas.springboot.di.app.conrollers;
 
 import com.bolsadeideas.springboot.di.app.models.domain.Factura;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
