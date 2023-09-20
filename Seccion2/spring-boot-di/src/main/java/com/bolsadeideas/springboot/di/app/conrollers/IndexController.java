54aff1e1c2091d8d2429e908de6ebc2d32353469
Isael Jimenez <isaeljimenez94@gmail.com>
2023-09-19 21:49:30 -0600

    Ejemplo con inyeccion de dependencia

--- Seccion2/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/conrollers/IndexController.java
+++ Seccion2/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/conrollers/IndexController.java
@@ -1,13 +1,17 @@
 package com.bolsadeideas.springboot.di.app.conrollers;
 
 import com.bolsadeideas.springboot.di.app.models.service.MiServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
-    private MiServicio servicio = new MiServicio();
+
+    @Autowired
+    private MiServicio servicio;
+
     @GetMapping({"/index", "/", ""})
     public String index(Model model) {
         String operacion = servicio.operacion();
--- Seccion2/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
+++ Seccion2/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicio.java
@@ -1,5 +1,9 @@
 package com.bolsadeideas.springboot.di.app.models.service;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Component
 public class MiServicio {
     public String operacion(){
         return "ejecutando algu proseso importante";
