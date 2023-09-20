fabca401bb4d8fd0c4d50230a68970c31ab9c06e
Isael Jimenez <isaeljimenez94@gmail.com>
2023-09-20 07:28:30 -0600

    agregando ora clase que implementa otra interfas @Primary

--- Seccion2/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicioComplejo.java
+++ Seccion2/spring-boot-di/src/main/java/com/bolsadeideas/springboot/di/app/models/service/MiServicioComplejo.java
@@ -1,14 +1,15 @@
 package com.bolsadeideas.springboot.di.app.models.service;
 
+
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 
-@Component("miServicioSimple")
-@Primary
-public class MiServicio implements IServicio {
+@Component("miServicioComplejo")
+
+public class MiServicioComplejo  implements IServicio{
+
     @Override
     public String operacion() {
-        return "ejecutando algun proceso importante simple";
+        return "ejecutando algun proceso importante complicado...";
     }
 }
