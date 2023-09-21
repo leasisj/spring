d88ad326568d83483513801c1daa23df86554c33
Isael Jimenez <isaeljimenez94@gmail.com>
2023-09-21 11:27:59 -0600

    Trabajando con formularios: @Getmaping y @PostMapin y @RequestParam

--- Seccion3/spring-boot-form/src/main/java/com/boldadeideas/springboot/form/app/controllers/FormController.java
+++ Seccion3/spring-boot-form/src/main/java/com/boldadeideas/springboot/form/app/controllers/FormController.java
@@ -4,17 +4,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
 
     @GetMapping("/form")//Este metodo es para mostrar el formulario en la pagina
     public String form(Model model) {
+        model.addAttribute("titulo", "Formulario de Cliente");
         return "form";
     }
 
     @PostMapping("/form")//Prosesa la informacion del formulario
-    public String procesar(Model model) {
+    public String procesar(Model model,
+                           @RequestParam String username,
+                           @RequestParam String password,
+                           @RequestParam String email
+
+    ) {
+        model.addAttribute("titulo", "Datos del formulario");
+        model.addAttribute("username", username);
+        model.addAttribute("password", password);
+        model.addAttribute("email", email);
         return "resultado"; //Se muestran los resutados que se evian atraves del formulario
         //Mas adelante este metodo va a mapear a la base de datos usando jpa
     }
