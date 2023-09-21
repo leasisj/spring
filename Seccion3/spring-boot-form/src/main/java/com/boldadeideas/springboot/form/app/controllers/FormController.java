c93d35d305952664cd6368bcf285bda3d4845659
Isael Jimenez <isaeljimenez94@gmail.com>
2023-09-21 15:08:34 -0600

    Agregando validaciones por medio de THymeleaf

--- Seccion3/spring-boot-form/src/main/java/com/boldadeideas/springboot/form/app/controllers/FormController.java
+++ Seccion3/spring-boot-form/src/main/java/com/boldadeideas/springboot/form/app/controllers/FormController.java
@@ -25,17 +25,8 @@ public class FormController {
     @PostMapping("/form")
     public String procesar(@Valid Usuarios usuario, BindingResult result, Model model) {
         model.addAttribute("titulo", "Datos del formulario");
-        //ver si es valido y ver si tiene error
-        //BindingResult result-> este es propio de spring y representa el resultado de la validacion y contiene los mensajes de error
-        //Esto pasa cuando el objeto esta anotado con @Valid.
 
         if (result.hasErrors()) {
-            //pasando errores a la vista
-            Map<String, String> errores = new HashMap<>();
-            result.getFieldErrors().forEach(err -> {
-                errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
-            });
-            model.addAttribute("error", errores);
             return "form"; //regresamos al formulario
         }
         model.addAttribute("usuario", usuario);
